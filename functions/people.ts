9a8a941bf0c3291de63f5ae06cfd8e7a39c5905a
s4nt14go <santiagopergamo@gmail.com>
2021-03-29 01:48:09 -0300

    fix: search pagination
    Increase lambdas' timeouts
    Add some logging

--- functions/people.ts
+++ functions/people.ts
@@ -1,17 +1,18 @@
 import { AppSyncResolverHandler } from "aws-lambda";
 import axios from 'axios'
 
-let url = 'https://swapi.dev/api/people/';
-
 export const handler: AppSyncResolverHandler<any, any> = async (event) => {
   console.log('event', event);
 
   const { page } = event.arguments;
   console.log('page', page);
 
+  let url = 'https://swapi.dev/api/people/';
   if (page) url += `?page=${page}`;
+  console.log('url', url);
 
   const { data } = await axios.get(url);
+  console.log('data', data);
 
   const { count, next, previous, results } = data;
 
--- functions/search.ts
+++ functions/search.ts
@@ -4,12 +4,16 @@ import axios from 'axios'
 export const handler: AppSyncResolverHandler<any, any> = async (event) => {
   console.log('event', event);
 
-  const { name } = event.arguments;
+  const { name, page } = event.arguments;
   console.log('name', name);
+  console.log('page', page);
 
-  const url = `https://swapi.dev/api/people/?search=${name}`;
+  let url = `https://swapi.dev/api/people/?search=${name}`;
+  if (page) url += `&page=${page}`;
+  console.log('url', url);
 
   const { data } = await axios.get(url);
+  console.log('data', data);
 
   const { count, next, previous, results } = data;
 
